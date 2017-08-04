e2e8bbc50d533e3fd24b6a2d177b2437dfce8248
VinayMavi <vinaymavi@gmail.com>
2017-08-04 11:59:42 +0530

    Routing working on cloud function.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my-stats-cloud-function
 My-stats-extension cloud function
+
+# Command to deploy
+gcloud beta functions deploy <NAME>  --stage-bucket beta-my-stats-ext.appspot.com --trigger-http 
--- index.js
+++ index.js
@@ -8,11 +8,44 @@
 'use strict';
 
 const Datastore = require('@google-cloud/datastore');
-
+const express = require('express');
+const myApp = express.Router({mergeParams:true});
 // Instantiates a client
 const datastore = Datastore();
+const KIND = "website";
 
 exports.set = function set(req, res) {
     console.log(req.body);
+    saveEntity(JSON.parse(req.body.value));
     res.status(200).send(JSON.stringify(req.body));
 };
+
+function saveEntity(data) {
+    console.log("Auto generated key = "+datastore.int(10).toString())
+    const entity = {
+        key: createKey(data.startTime),
+        data: data
+    }
+
+    datastore.save(entity)
+        .then(()=> {
+            console.log(entity + "Saved.")
+        })
+        .catch(()=> {
+            console.error("Error to save entity.")
+        });
+}
+
+function createKey(key) {
+    return datastore.key([KIND, key])
+}
+
+myApp.get("/sub",function (req, resp) {
+    resp.send("This is sub");
+});
+myApp.all("*",function (req, resp) {
+    resp.send("This is ********");
+});
+
+
+exports.myroute = myApp;
